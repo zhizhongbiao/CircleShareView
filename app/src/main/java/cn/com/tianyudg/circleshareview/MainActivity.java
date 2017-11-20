5c6055a0f96a42bb051572480367bc98dae2e2c5
WaterFlower <zhizhongbiao163@163.com>
2017-11-20 14:25:04 +0800

    add BrokenLineStatisticsTableView

--- app/src/main/java/cn/com/tianyudg/circleshareview/MainActivity.java
+++ app/src/main/java/cn/com/tianyudg/circleshareview/MainActivity.java
@@ -1,12 +1,13 @@
 package cn.com.tianyudg.circleshareview;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.Arrays;
 import java.util.List;
 
+import cn.com.tianyudg.circleshareview.view.BrokenLineStatisticsTableView;
 import cn.com.tianyudg.circleshareview.view.CircleShareView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,9 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         CircleShareView circleShareView = (CircleShareView) findViewById(R.id.csv);
+        BrokenLineStatisticsTableView blst = (BrokenLineStatisticsTableView) findViewById(R.id.blst);
         List<Integer> integers = Arrays.asList(Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW);
         List<Float> floats = Arrays.asList(0.1f, 0.4f, 0.3f,0.2f);
-
+        List<String> xSubjects = Arrays.asList("十月", "十月","十月","十月","十月","十月","十月");
+        List<Integer> ySubjects = Arrays.asList(1542, 2455, 345, 454, 5245, 642, 7424);
+        blst.setData(xSubjects,ySubjects);
         circleShareView.setShareAndColor(floats,integers);
     }
 }
